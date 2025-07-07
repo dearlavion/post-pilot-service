f53bd689dd3702116fac2b3535756670b2df3db6
dearlavion <dearlavion@gmail.com>
2025-07-08 00:08:11 +0800

    BASE CODE

---
+++ README.md
@@ -0,0 +1,4 @@
+Post Pilot
+# Post Pilot
+
+<img src="https://dear.lavion/images/post-pilot.png" width="250">
---
+++ src/main/java/lavion/dear/PostPilotApplication.java
@@ -0,0 +1,12 @@
+package lavion.dear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostPilotApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(PostPilotApplication.class, args);
+    }
+}
